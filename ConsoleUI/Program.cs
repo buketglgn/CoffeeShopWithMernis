4a1f648bd730bc71c092bcf9065728f581a481dd
Buket Gulgun <buketglgn@gmail.com>
2021-02-09 23:10:52 +0300

    .

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -14,6 +14,7 @@ namespace ConsoleUI
            StarbucksCustomerManager customerManager = new StarbucksCustomerManager(new MernisServiceAdapter(), new EfCustomerDal());
 
             customerManager.Add(new Customer { DateOfBirth = new DateTime(1996, 12, 23), FirstName = "sfsg", LastName = "fsf", NationaltyIdentity = "34356" }) ;
+
         }
     }
 }
